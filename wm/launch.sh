9af661e5aefdd5aca448a78da06ee13c8db1b187
Blottiere Paul <blottiere.paul@gmail.com>
2021-04-26 23:22:26 +0200

    Update polybar config

--- wm/launch.sh
+++ wm/launch.sh
@@ -1,5 +1,14 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
+# Add this script to your wm startup file.
+
+DIR="$HOME/.config/polybar/"
+
+# Terminate already running bar instances
 killall -q polybar
+
+# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar --config=$HOME/.config/polybar/config main &
+
+# Launch the bar
+polybar -q main -c "$DIR"/polybar.ini &
