63b5381e22904269598788736d3afe0d13613ffa
Blottiere Paul <blottiere.paul@gmail.com>
2021-10-27 23:25:22 +0200

    Add custom macro

--- kb/keymap.c
+++ kb/keymap.c
@@ -1,29 +1,37 @@
 #include QMK_KEYBOARD_H
 
+#define LOWER   MO(_LOWER)
+
+enum custom_keycodes {
+    M_GUI,
+};
+
 enum layers {
     _MAIN,
-    _RAISE,
     _LOWER,
 };
 
-// Readability keycodes
-#define LOWER   MO(_LOWER)
-#define RAISE   MO(_RAISE)
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode){
+    case M_GUI:
+      if(record->event.pressed){
+        layer_on(_LOWER);
+        register_mods(MOD_LGUI);
+      } else {
+        layer_off(_LOWER);
+        unregister_mods(MOD_LGUI);
+      }
+      return false;
+  }
+  return true;
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
   [_MAIN] = LAYOUT_ortho_4x12(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_DEL,  KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_PGUP, KC_UP,   KC_PGDOWN,
-    KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_DOT,  KC_LEFT, KC_DOWN, KC_RGHT
-  ),
-
-  [_RAISE] = LAYOUT_ortho_4x12(
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-  RESET,   _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    KC_LCTL, M_GUI,   KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_DOT,  KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_LOWER] = LAYOUT_ortho_4x12(
@@ -32,5 +40,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
   _______, _______, _______, _______, KC_NO,   KC_NO,   KC_NO,   KC_NO,   _______, _______, _______, _______
   )
-
 };
