0a2f8b27467f4895ab9730783da2ae0274600a0a
Blottiere Paul <blottiere.paul@gmail.com>
2022-02-15 19:51:02 +0100

    Add more key

--- kb/keymap.c
+++ kb/keymap.c
@@ -59,24 +59,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NUBS,
-    KC_LCTL, M_GUI,   KC_LGUI, BLUE,   _GREEN,  KC_SPC,  KC_SPC,   KC_ALGR,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, M_GUI,   KC_LGUI, BLUE,    _GREEN,  KC_SPC,  KC_SPC,  KC_ALGR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   ),
 
 /* Blue
  * ,-----------------------------------------------------------------------------------.
  * | &    |   é  |   "  |   '  |   (  |   -  |   è  |   _  |   ç  |   à  |   )  | =    |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |Enter |
+ * |      |      |      |      |      |      |      |      |      |      |      |  $   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |      |      |      |      |      |  *   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      | GUI2 | GUI  | Blue |Green |    Space    |Black | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
   [_BLUE] = LAYOUT_ortho_4x12(
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_RBRC,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BSLS,
   _______, _______, _______, _______, KC_NO,   KC_NO,   KC_NO,   KC_RALT, _______, _______, _______, _______
   )
 
