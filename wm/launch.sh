b6e94777fa497caeddde367ad1f66a884e8910f8
Blottiere Paul <blottiere.paul@gmail.com>
2023-07-06 20:23:04 +0200

    Add terminology theme

--- wm/launch.sh
+++ wm/launch.sh
@@ -11,4 +11,12 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
-polybar -q main -c "$DIR"/polybar.ini &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    echo $m
+    MONITOR=$m polybar -q main -c "$DIR"/polybar.ini &
+  done
+else
+  polybar -q main -c "$DIR"/polybar.ini &
+  # polybar --reload example &
+fi
